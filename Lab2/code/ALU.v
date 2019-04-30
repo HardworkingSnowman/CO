c14c1c76b01087d61222097a951df5a13fbfef59
root <root@DESKTOP-CHN3BSE.localdomain>
2019-04-30 11:47:47 +0800

    ALU.v finish

--- Lab2/code/ALU.v
+++ Lab2/code/ALU.v
@@ -32,6 +32,28 @@ wire             zero_o;
 //Parameter
 
 //Main function
+always@(*)
+begin
+	case(ctrl_i)
+		//and
+		4'b0000 : result_o = src1_i & src2_i;
+		//ori, or
+		4'b0001 : result_o = src1_i | src2_i;
+		//addu, addi
+		4'b0010 : result_o = src1_i + src2_i;
+		//beq, subu, bne
+		4'b0110 : result_o = src1_i - src2_i;
+		//sltu
+		4'b0111 : result_o = (src1_i < src2_i) ? 1 : 0;
+		//slt
+		4'b1000 : result_o = ($signed(src1_i) < $signed(src2_i)) ? 1 : 0;
+		//sra, srav
+		4'b1001 : result_o = src1_i >> src2_i;
+		//lui
+		4'b1011 : result_o = src2_i << 16;
+		end
+	endcase
+end
 
 endmodule
 
