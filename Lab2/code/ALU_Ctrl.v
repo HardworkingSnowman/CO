65f8e4d8d3d4a051a7c600960ee9881cf2d998af
che-chuan chang <ryanchang.cs06@nctu.edu.tw>
2019-04-30 15:07:08 +0000

    rrr

--- Lab2/code/ALU_Ctrl.v
+++ Lab2/code/ALU_Ctrl.v
@@ -36,13 +36,14 @@ begin
   else if(ALUOp_i == 3'b101) ALUCtrl_o = 4'b1011; //lui
   else if(ALUOp_i == 3'b110) ALUCtrl_o = 4'b0001; //ori
   else if(ALUOp_i == 3'b011) ALUCtrl_o = 4'b0110; //bne
+  else if(ALUOp_i == 3'b111) ALUCtrl_o = 4'01111; //sltiu
   
   else if({ALUOp_i, funct_i} == 9'b010100001) ALUCtrl_o = 4'b0010; //addu
   else if({ALUOp_i, funct_i} == 9'b010100011) ALUCtrl_o = 4'b0110; //subu
   else if({ALUOp_i, funct_i} == 9'b010100100) ALUCtrl_o = 4'b0000; //and
   else if({ALUOp_i, funct_i} == 9'b010100101) ALUCtrl_o = 4'b0001; //or
   else if({ALUOp_i, funct_i} == 9'b010101010) ALUCtrl_o = 4'b1000; //slt
-  else if({ALUOp_i, funct_i} == 9'b010101011) ALUCtrl_o = 4'b0111; //sltu
+  //else if({ALUOp_i, funct_i} == 9'b010101011) ALUCtrl_o = 4'b0111; //sltu
   else if({ALUOp_i, funct_i} == 9'b010000011) ALUCtrl_o = 4'b1001; //sra
   else if({ALUOp_i, funct_i} == 9'b010000111) ALUCtrl_o = 4'b1001; //srav
   else ALUCtrl_o = 4'b1111;
